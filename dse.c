39c7b07bcae7cdedb2c896514d50ce23fd2cc4ea
rezado <1908819406@qq.com>
2025-02-25 10:49:50 +0800

    support RASSize dynamic parameter adjustment

--- dse.c
+++ dse.c
@@ -58,6 +58,7 @@ void main() {
         *(volatile uint64_t *)L3SETS0_REG = *(volatile uint64_t *)L3SETS_ADDR;
         *(volatile uint64_t *)INTPHYREGS0_REG = *(volatile uint64_t *)INTPHYREGS_ADDR;
         *(volatile uint64_t *)FPPHYREGS0_REG = *(volatile uint64_t *)FPPHYREGS_ADDR;
+        *(volatile uint64_t *)RASSIZE0_REG = *(volatile uint64_t *)RASSIZE_ADDR;
     } else {
         *(volatile uint64_t *)ROBSIZE1_REG = *(volatile uint64_t *)ROBSIZE_ADDR;
         *(volatile uint64_t *)LQSIZE1_REG = *(volatile uint64_t *)LQSIZE_ADDR;
@@ -73,6 +74,7 @@ void main() {
         *(volatile uint64_t *)L3SETS1_REG = *(volatile uint64_t *)L3SETS_ADDR;
         *(volatile uint64_t *)INTPHYREGS1_REG = *(volatile uint64_t *)INTPHYREGS_ADDR;
         *(volatile uint64_t *)FPPHYREGS1_REG = *(volatile uint64_t *)FPPHYREGS_ADDR;
+        *(volatile uint64_t *)RASSIZE1_REG = *(volatile uint64_t *)RASSIZE_ADDR;
     }
 
     // test
